367aeb12f9951ab625d129b13a09230d8b0d82e5
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-15 15:22:43 -0700

    Red and black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Your JS goes here
+
+function checker(){
+
+  var squareb = [];
+
+  for (var i = 0; i < 63; i++) {
+
+    squareb[i] = document.createElement('div');
+    document.body.appendChild(squareb[i]);
+    squareb[i].style.width = '11.1%';
+    squareb[i].style.paddingBottom = '11.1%'
+    squareb[i].style.float = 'left';
+    if (i%2===0){
+      squareb[i].style.backgroundColor = 'black';
+    }else{squareb[i].style.backgroundColor = 'red';
+  }
+}
+}
+checker();
